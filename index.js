af24189fe7e8d21a6971f09eacf4d3c65af4cbc8
Fredy Papa <fredy.luis.wolf@gmail.com>
2022-03-14 19:59:15 -0500

    Segundo commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 let express = require('express');
 let app = express();
 let PORT = 3004;
@@ -9,3 +10,19 @@ app.get("/",(req,res)=>{
 app.listen(PORT,()=>{
     console.log("Server on");
 })
+*/
+
+class Operaciones{
+    suma(num1,num2){
+        return num1+num2;
+    }
+    resta(num1,num2){
+        return num1-num2;
+    }
+    multiplicacion(num1,num2){
+        return num1*num2;
+    }
+    division(num1,num2){
+        return num1/num2;
+    }
+}
